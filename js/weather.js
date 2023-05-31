257ef4719abe4a047440e192e721695a01712017
Dieison Lemes Vieira <dieisonambiental@gmail.com>
2023-05-31 10:46:32 -0300

    Add files via upload

---
+++ js/weather.js
@@ -0,0 +1,30 @@
+const APIKey = '6H0xGsYQm44DCaldgdhGGO28FLf5ZQQm'
+const baseUrl = 'https://dataservice.accuweather.com/'
+
+const getCityUrl = cityName => 
+    `${baseUrl}locations/v1/cities/search?apikey=${APIKey}&q=${cityName}`
+
+const getWeatherUrl = cityKey => 
+    `${baseUrl}currentconditions/v1/${cityKey}?apikey=${APIKey}&language=pt-br`
+
+const fetchData = async url => {
+    try {
+        const response = await fetch(url)
+
+        if (!response.ok) {
+            throw new Error('Não foi possível obtrer os dados')
+        }
+
+        return response.json()
+
+    } catch ({ name, message}) {
+        alert(`${name}: ${message}`)            
+
+    }
+}
+
+const getCityData = cityName => fetchData(getCityUrl(cityName))
+
+ const getCityWeather = cityKey => fetchData(getWeatherUrl(cityKey))
+
+
